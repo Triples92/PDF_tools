bd0db5af92354ebabdd66c693436daed6a938032
Sari Sarieddine <sari.sarieddine@gmail.com>
2024-09-23 13:15:41 -0400

    added functionality to name output file

--- pdf_merge.py
+++ pdf_merge.py
@@ -3,13 +3,13 @@
 from pypdf import PdfWriter
 import sys
 
-
-file1 = sys.argv[1]
-file2 = sys.argv[2]
-file3 = sys.argv[3] if sys.argv[3] != "None" else None
-file4 = sys.argv[4] if sys.argv[4] != "None" else None
-file5 = sys.argv[5] if sys.argv[5] != "None" else None
-file6 = sys.argv[6] if sys.argv[6] != "None" else None
+output_name = sys.argv[1]
+file1 = sys.argv[2]
+file2 = sys.argv[3]
+file3 = sys.argv[4] if sys.argv[4] != "None" else None
+file4 = sys.argv[5] if sys.argv[5] != "None" else None
+file5 = sys.argv[6] if sys.argv[6] != "None" else None
+file6 = sys.argv[7] if sys.argv[7] != "None" else None
 
 file_list = [file1,file2,file3,file4,file5,file6]
 merger = PdfWriter()
@@ -20,5 +20,5 @@ for pdf in file_list:
     else:
         merger.append(pdf)
 
-merger.write("merged-pdf.pdf")
+merger.write(output_name)
 merger.close()
